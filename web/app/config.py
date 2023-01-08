d3d2465283fc513b2240647614ac4dd51108db00
v279e <71925316+v279e@users.noreply.github.com>
2023-01-08 16:51:35 +0300

    Update config.py

--- web/app/config.py
+++ web/app/config.py
@@ -1,6 +1,6 @@
 SECRET_KEY = 'Spartak2023'
 
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://postgres:1234@192.168.1.179:54321/my_db'
+SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://postgres:1234@192.168.1.214:54321/my_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 

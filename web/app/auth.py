38c1c64da48ef36cbdf5d3acff356c29ee997f34
v279e <71925316+v279e@users.noreply.github.com>
2023-01-09 23:57:26 +0300

    Update auth.py

--- web/app/auth.py
+++ web/app/auth.py
@@ -6,10 +6,6 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-
-
 def init_login_manager(app):
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'

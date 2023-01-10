3c5b5d635000c61edaf736272eb274446065bcad
v279e <71925316+v279e@users.noreply.github.com>
2023-01-10 15:50:21 +0300

    Update analiz.py

--- web/analiz.py
+++ web/analiz.py
@@ -2,7 +2,7 @@ import os
 import json
 pylint = os.popen('pylint ./app').read()
 if pylint:
-    print("Ошибки качества кода")
+     raise RuntimeError('Ошибки качества кода')
 
 os.system('bandit -iii -lll -q -r ./app/ -o bandit.json -f json ')
 with open('bandit.json', 'r') as f:

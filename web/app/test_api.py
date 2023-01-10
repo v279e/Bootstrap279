2d5b0c82350a8266197f14df1915b191561153b8
v279e <71925316+v279e@users.noreply.github.com>
2023-01-10 20:45:26 +0300

    Create test_api.py

---
+++ web/app/test_api.py
@@ -0,0 +1,27 @@
+import pytest
+from app import client
+
+
+def test_index():
+    response = client.get('/')
+    assert response.status_code == 200
+    
+def test_login():
+    response = client.get('/auth/login')
+    assert response.status_code == 200
+
+def test_valid_auth():
+    data={"login": "v279e",
+          "password" : "user"}
+    response = client.post('/auth/login', data=data)
+    assert response.status_code == 302
+
+def test_invalid_auth():
+    data={"login": "v279e",
+          "password" : "user256"}
+    response = client.post('/auth/login', data=data)
+    assert response.status_code == 200
+
+def test_logout():
+    response=client.get('/auth/logout')
+    assert response.status_code==302
